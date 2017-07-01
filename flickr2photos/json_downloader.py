baf242cac1ad6e51f75f034852e8e64858346727
Anderson Mesquita <andersonvom@gmail.com>
2017-07-01 16:29:36 -0400

    Add class to download all image json info from Flickr
    This class is responsible for downloading all image information in JSON
    format from every Flickr set.

---
+++ flickr2photos/json_downloader.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+from .flickr_client import FlickrClient
+from .error_handler import handle
+
+
+BASE_DIR = 'json_files'
+MAX_RETIES = 5
+
+
+class JsonDownloader(object):
+    def __init__(self):
+        print('Starting flickr')
+        self.client = FlickrClient()
+        print('Initialized')
+
+    def start(self):
+        for info in self.client.walk_photos():
+            handle(process_json, **info)
+
+
+def process_json(set_info, photo):
+    set_title = set_info['title']['_content']
+    photo_title = photo['title']
+    url = photo['url_o']
+
+    dir_path = os.path.join(BASE_DIR, set_title)
+    file_path = get_json_name(dir_path, photo)
+    os.makedirs(dir_path, exist_ok=True)
+    if os.path.isfile(file_path):
+        print('ERROR-FileExists:' + file_path)
+    json.dump(photo, open(file_path, 'w'))
+    print('Saved: %s - %s' % (set_title, file_path))
+
+def get_json_name(dir_path, photo):
+    return os.path.join(dir_path, photo['id']) + '.json'
