f24cdfe3f845f2bb37c9d86429479432703fd057
Anderson Mesquita <andersonvom@gmail.com>
2017-07-01 17:30:29 -0400

    Reduce verbosity on save photos

--- save_photos.py
+++ save_photos.py
@@ -5,8 +5,8 @@ from tasks.save_photos import process_photo
 
 def start():
     for dirname, subdirs, filenames in os.walk('json_files'):
+        print('Queueing: %s' % dirname)
         for filename in filenames:
-            print('%s/%s' % (dirname, filename))
             process_photo.delay('%s/%s' % (dirname, filename))
 
 
